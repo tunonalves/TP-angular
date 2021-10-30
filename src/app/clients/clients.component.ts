6c474672aaa3bf013d5ae67ecf5ea1911a3310bd
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-10-30 19:11:41 -0300

    tp

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -12,6 +12,51 @@ export class ClientsComponent implements OnInit {
   dni: number = 0;
   email: string = "";
   address: string = "";
+  
+  //GET
+  getclientid(){
+    return this.clientID;
+  }
+  getfirtname(){
+    return this.firstName;
+  }
+  getlastname(){
+    return this.lastName;
+  }
+  getdni(){
+    return this.dni;
+  }
+  getemail(){
+    return this.email;
+  }
+  getaddress(){
+    return this.address;
+  }
+  //SET
+  setclientid(){
+    this.clientID;
+  }
+  setfirtname(){
+    this.firstName;
+  }
+  setlastname(){
+    this.lastName;
+  }
+  setdni(){
+    this.dni;
+  }
+  setemail(){
+    this.email;
+  }
+  setaddress(){
+    this.address;
+  }
+
+  //METHOD
+  addclient(){
+    alert(this.clientID+" "+this.firstName+" "+this.lastName+" "+this.dni+" "+this.email+" "+this.address);
+  }
+
 
   constructor() {
 

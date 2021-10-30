b240b93e0a1f0e522890c72d7d0fa64b8deb0da7
Federico Tuñon Alves <70298964+tunonalves@users.noreply.github.com>
2021-10-30 19:38:34 -0300

    TP7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ClientAddCoponentComponent } from './client-add-coponent/client-add-coponent.component';
 import { ClientLitComponentComponent } from './client-lit-component/client-lit-component.component';
-import { ClientsComponent } from './clients/clients.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientAddCoponentComponent,
-    ClientLitComponentComponent,
-    ClientsComponent
+    ClientLitComponentComponent
   ],
   imports: [
     BrowserModule,
--- src/app/client-add-coponent/client-add-coponent.component.ts
+++ src/app/client-add-coponent/client-add-coponent.component.ts
@@ -6,7 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-add-coponent.component.css']
 })
 export class ClientAddCoponentComponent implements OnInit {
+  clientID: number = 0;
+  firstName: string = "";
+  lastName: string = "";
+  dni: number = 0;
+  email: string = "";
+  address: string = "";
+  
+  //GET
+  getclientid(){
+    return this.clientID;
+  }
+  getfirtname(){
+    return this.firstName;
+  }
+  getlastname(){
+    return this.lastName;
+  }
+  getdni(){
+    return this.dni;
+  }
+  getemail(){
+    return this.email;
+  }
+  getaddress(){
+    return this.address;
+  }
+  //SET
+  setclientid(){
+    this.clientID;
+  }
+  setfirtname(){
+    this.firstName;
+  }
+  setlastname(){
+    this.lastName;
+  }
+  setdni(){
+    this.dni;
+  }
+  setemail(){
+    this.email;
+  }
+  setaddress(){
+    this.address;
+  }
 
+  //METHOD
+  addclient(){
+    alert(this.clientID+" "+this.firstName+" "+this.lastName+" "+this.dni+" "+this.email+" "+this.address);
+  }
   constructor() { }
 
   ngOnInit(): void {
